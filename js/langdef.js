9b580a1a053a4024260b423b7199bade94d6ebba
André <alcbcoelho@gmail.com>
2022-07-25 11:43:44 -0300

    Finished and tidied up the core code; created scss file on top of previous stylesheet.

---
+++ js/langdef.js
@@ -0,0 +1,89 @@
+"use strict";
+
+let lang = "en";
+const txt = {
+    lang: {
+        en: "en-US",
+        pt: "pt-BR"
+    },
+    pageTitle: {
+        en: "Shopping List Maker",
+        pt: "Criador de Lista de Compras"
+    },
+    productFieldPlaceholder: {
+        en: "Type in your product",
+        pt: "Digite seu produto"
+    },
+    amountFieldPlaceholder: {
+        en: "Qty.",
+        pt: "Qtd."
+    },
+    productFieldTitle: {
+        en: "Your product (e.g. milk)",
+        pt: "Seu produto (exemplo: leite)"
+    },
+    amountFieldTitle: {
+        en: "The desired quantity of the specified product (if blank, a single item will be computed)",
+        pt: "A quantidade desejada do produto especificado (se deixado em branco, será computado um único item)"
+    },
+    formattingInnerText: {
+        en: "Formatting:",
+        pt: "Formatação:"
+    },
+    formatBtnUnorderedListTitle: {
+        en: "Bullet point formatting",
+        pt: "Formatação com marcadores"
+    },
+    formatBtnOrderedListTitle: {
+        en: "Numbered formatting",
+        pt: "Formatação com números"
+    },
+    btnAddItemInnerText: {
+        en: "Add",
+        pt: "Adicionar"
+    },
+    btnClearListInnerText: {
+        en: "Clear list",
+        pt: "Limpar lista"
+    },
+    btnYesInnerText: {
+        en: "Yes",
+        pt: "Sim"
+    },
+    btnNoInnerText: {
+        en: "No",
+        pt: "Não"
+    },
+    btnMoveItemUpTitle: {
+        en: "Move item up",
+        pt: "Mover item para cima"
+    },
+    btnMoveItemDownTitle: {
+        en: "Move item down",
+        pt: "Mover item para baixo"
+    },
+    btnRemoveItemTitle: {
+        en: "Remove item",
+        pt: "Remover item"
+    },
+    modalMsg: {
+        en: [
+            "There is already a product with this name on the list. Insert it anyway?",
+            "Your list will be emptied out. Do you wish to continue?"
+        ],
+        pt: [
+            "Já existe um produto com esse nome na lista. Inserir mesmo assim?",
+            "Sua lista será completamente esvaziada. Deseja continuar?"
+        ]
+    },
+    alertMsg: {
+        en: [
+            "Please insert your product in the appropriate field above.",
+            "There are no items to remove!"
+        ],
+        pt: [
+            "Por favor, insira seu produto no espaço apropriado acima.",
+            "Não há itens para remover!"
+        ]
+    },
+};
--- js/script.js
+++ js/script.js
@@ -1,364 +1,308 @@
-const body = document.querySelector("body");
+"use strict";
+// import txt from langdef.js;
+
+/////////////////////////////////////////////////
+// VARIABLES
+/////////////////////////////////////////////////
+
+// input field elements
 const productField = document.querySelector("#product-field");
 const amountField = document.querySelector("#amount-field");
-const itemType = document.querySelector("#item-type");
+
+// div elements
 const alertMsg = document.querySelector(".alert");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const listContainer = document.querySelector(".list");
+
+// button elements
 const btnAddItem = document.querySelector("#add-item");
 const btnClearList = document.querySelector("#clear-list");
 const btnUl = document.querySelector("#unordered-list");
 const btnOl = document.querySelector("#ordered-list");
 const btnYes = document.querySelector("#yes");
 const btnNo = document.querySelector("#no");
-const listContainer = document.querySelector(".list");
 
-const alertMsgClasses = alertMsg.classList;
+// messages
+const modalMsgClearList = "Sua lista será completamente esvaziada. Deseja continuar?";
 
-let items = [];
-let createNewList = true;
+// lets
+let itemsData = [];
+let itemIDGen = 0;
 let allowDuplicate = false;
 
-function drawButtons() {
-  const liBtns = document.querySelectorAll(".li-btn");
-  console.log("Li buttons previously created:", liBtns);
-  liBtns.forEach((btn) => btn.remove());
+/////////////////////////////////////////////////
+// FUNCTIONS
+/////////////////////////////////////////////////
 
-  const btnMoveItemUp = document.createElement("button");
-  const btnMoveItemDown = document.createElement("button");
-  const btnRemoveItem = document.createElement("button");
-  const lis = listContainer.querySelectorAll("li");
-  // const liBtns_ = [btnMoveItemUp, btnMoveItemDown, btnRemoveItem];
-
-  let btnBlueprints = []; // li button array
-
-  // button setup
-  btnMoveItemUp.innerHTML = "&#8679";
-  btnMoveItemUp.title = "Mover item para cima";
-  btnMoveItemUp.classList.add("li-btn", "move-item", "visibility-hidden");
-
-  btnMoveItemDown.innerHTML = "&#8681";
-  btnMoveItemDown.title = "Mover item para baixo";
-  btnMoveItemDown.classList.add("li-btn", "move-item", "visibility-hidden");
-
-  btnRemoveItem.innerHTML = "&times;";
-  btnRemoveItem.title = "Remover item";
-  btnRemoveItem.classList.add("li-btn", "remove-item", "visibility-hidden");
-
-  /*   items.forEach((item, index) => {
-    // console.log(item, index);
-    item = 0;
-  }); */
-  btnBlueprints = items.map((item, i) => {
-    switch (i) {
-      case 0:
-        if (items.length === 1) {
-          return "[x]";
-        } else if (items.length > 1) {
-          return "[v][x]";
-        }
-        break;
-      case items.length - 1: // && index !== 0
-        return "[^][x]";
-        break;
-      default:
-        return "[^][v][x]";
-    }
-  });
-  console.log("btnBlueprints:", btnBlueprints, "\nlis: ", lis);
-  lis.forEach((li, i) => {
-    // function
-    function toggleVisibility(onoff) {
-      switch (onoff) {
-        case 0:
-          btnMoveItemUp.classList.add("visibility-hidden");
-          btnMoveItemDown.classList.add("visibility-hidden");
-          btnRemoveItem.classList.add("visibility-hidden");
-          break;
-        case 1:
-          btnMoveItemUp.classList.remove("visibility-hidden");
-          btnMoveItemDown.classList.remove("visibility-hidden");
-          btnRemoveItem.classList.remove("visibility-hidden");
-          break;
-      }
-    }
-    //
-    btnBlueprints.forEach((btn) => {
-      switch (btn) {
-        case "[x]":
-          li.append(btnRemoveItem);
-          break;
-        case "[v][x]":
-          li.append(btnMoveItemDown, btnRemoveItem);
-          break;
-        case "[^][x]":
-          li.append(btnMoveItemUp, btnRemoveItem);
-          break;
-        case "[^][v][x]":
-          li.append(btnMoveItemUp, btnMoveItemDown, btnRemoveItem);
-          break;
-      }
-    });
-    /*     btnBlueprints.forEach((btn) => {
-      btn.forEach((btn_) => {
-        li.append(btn_);
-      });
-    }); */
-    /*     if (li.getElementsByClassName("li-btn").length === 0) {
-    // only append buttons once per li
-    } */
-
-    li.addEventListener("mouseover", () => toggleVisibility(1));
-    li.addEventListener("mouseleave", () => toggleVisibility(0));
-
-    /*     btnMoveItemDown.addEventListener("click", () => {
-      let arr = [];
-
-      lis.forEach((element) => {
-        arr.push(element.innerHTML);
-      });
-
-      const j = arr.indexOf(li.innerHTML);
-      const [x, y] = [arr[j], arr[j + 1]];
-      lis[j].innerHTML = y;
-      lis[j + 1].innerHTML = x;
-    }); //
-
-    btnMoveItemUp.addEventListener("click", () => {
-      let arr = [];
-      const lis = document.querySelectorAll("li");
-
-      lis.forEach((element) => {
-        arr.push(element.innerHTML);
-      });
-
-      const j = arr.indexOf(li.innerHTML);
-      const [x, y] = [arr[j - 1], arr[j]];
-      lis[j - 1].innerHTML = y;
-      lis[j].innerHTML = x;
-    }); //
-
-    btnRemoveItem.addEventListener("click", () => {
-      // const i = items.indexOf(p);
-      const l = document.querySelector("#shoplist");
-      // newList.remove();
-      li.remove();
-      if (l.children.length === 0) {
-        l.remove();
-        closeList();
-      }
-      // items.splice(i, 1);
-      // console.log("ITEMS:", items);
-    }); */
-  });
-}
-
-function addItem() {
-  const p = productField.value;
-
-  if (items.includes(p) && allowDuplicate === false) {
-    modal.classList.remove("hidden"); // "Já existe um produto com esse nome na lista. Inserir mesmo assim?"
+const openModal = function (
+  modalMsg = "Já existe um produto com esse nome na lista. Inserir mesmo assim?"
+) {
+  modal.querySelector("p").innerText = modalMsg;
+  if (modal.classList.contains("hidden")) {
+    modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
-  } else {
-    const listFormat = btnUl.classList.contains("format-btn-selected")
-      ? "ul"
-      : "ol";
-    /* document.querySelector("#unordered-list").checked === true ? "ul" : "ol"; */
-
-    // create elements
-    const newList = document.createElement(listFormat); // = ul | ol
-    const existingList = document.querySelector("#shoplist");
-    const li = document.createElement("li");
-    /* const btnMoveItemUp = document.createElement("button");
-    const btnMoveItemDown = document.createElement("button");
-    const btnRemoveItem = document.createElement("button");
-    const liBtns_ = [btnMoveItemUp, btnMoveItemDown, btnRemoveItem]; */
-
-    const amount = Number(amountField.value) <= 0 ? "1" : amountField.value;
-    const product = amount === "" || amount === "1" ? p : `${p} (x${amount})`;
-
-    // button setup
-
-    //
-    if (p === "") {
-      // alertMsgClasses.remove("hidden"); // show alert if product field is empty
-      alertMsg.innerHTML =
-        "<p>Por favor, insira seu produto no espaço apropriado acima.</p><br>";
-    } else {
-      alertMsg.innerHTML = "";
-      if (p !== "") items.push(p); // items++
-      if (listContainer.classList.contains("hidden"))
-        listContainer.classList.remove("hidden");
-
-      // build up list
-      li.append(product);
-      if (existingList === null) {
-        newList.id = "shoplist";
-        newList.append(li);
-        listContainer.append(newList); // append newly created list to container
-        console.log(`existingList: ${existingList}
-        existingList_: ${document.querySelector("#shoplist")}`); //
-      } else {
-        existingList.append(li);
-      }
-    }
-
-    /* ========================================================
-    li.addEventListener("mouseover", () => {
-      for (let i = 0; i < liBtns_.length; i++) {
-        liBtns_[i].classList.remove("visibility-hidden");
-      }
-    });
-
-    li.addEventListener("mouseleave", () => {
-      for (let i = 0; i < liBtns_.length; i++) {
-        liBtns_[i].classList.add("visibility-hidden");
-      }
-    });
-
-    btnMoveItemDown.addEventListener("click", () => {
-      let arr = [];
-      const listElements = document.querySelectorAll("li");
-
-      listElements.forEach((element) => {
-        arr.push(element.innerHTML);
-      });
-
-      const i = arr.indexOf(li.innerHTML);
-      const [x, y] = [arr[i], arr[i + 1]];
-      listElements[i].innerHTML = y;
-      listElements[i + 1].innerHTML = x;
-    }); //
-
-    btnMoveItemUp.addEventListener("click", () => {
-      let arr = [];
-      const listElements = document.querySelectorAll("li");
-
-      listElements.forEach((element) => {
-        arr.push(element.innerHTML);
-      });
-
-      const i = arr.indexOf(li.innerHTML);
-      const [x, y] = [arr[i - 1], arr[i]];
-      listElements[i - 1].innerHTML = y;
-      listElements[i].innerHTML = x;
-    }); //
-
-    btnRemoveItem.addEventListener("click", () => {
-      const i = items.indexOf(p);
-      const l = document.querySelector("#shoplist");
-      // newList.remove();
-      li.remove();
-      if (l.children.length === 0) {
-        l.remove();
-        closeList();
-      }
-      items.splice(i, 1);
-      console.log("ITEMS:", items); //
-    });
-    ======================================================== */
-
-    // if (p !== "") items.push(p);
-    drawButtons();
-
-    // show info
-    console.log(
-      `=== INFO ===
-  
-    allowDuplicate: ${allowDuplicate}
-    existingList: ${existingList}
-    ITEMS:`,
-      items
-    );
   }
-}
+};
 
-/* const displayAlertMessage = function () {
-
-} */
-
-function closeModal() {
+const closeModal = function () {
   if (!modal.classList.contains("hidden")) {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
   }
-}
+};
 
-function closeList() {
+const closeList = function () {
   if (!listContainer.classList.contains("hidden"))
     listContainer.classList.add("hidden");
-}
+};
+
+const reformatList = function (type) {
+  try {
+    if (type !== "ul" && type !== "ol")
+      throw new Error("Argument 'type' must be set to either 'ol' or 'ul'.");
+
+    const existingList = document.querySelector("#shoplist");
+    const reverseType = type === "ul" ? "ol" : "ul";
+
+    if (existingList && existingList.tagName.toLowerCase() === reverseType) {
+      const newListType = document.createElement(type);
+      newListType.id = existingList.id;
+      itemsData.forEach((item) => newListType.append(item.li));
+      listContainer.replaceChildren(newListType);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const addItem = function () {
+  ///////////////////////////////////////////////
+  // INNER FUNCTIONS
+  ///////////////////////////////////////////////
+
+  const moveItemUp = function (li) {
+    const i = itemsData.findIndex((item) => item.itemID === li.id);
+    const existingList = document.querySelector("#shoplist");
+
+    if (i !== 0) {
+      const [movingDown, movingUp] = [itemsData[i - 1], itemsData[i]];
+      itemsData[i] = movingDown;
+      itemsData[i - 1] = movingUp;
+    }
+
+    existingList.innerHTML = "";
+    itemsData.forEach((item) => existingList.append(item.li));
+
+    console.log(
+      "ID of li to move up:",
+      li.id,
+      "\nIndex of li to move up:",
+      i,
+      "\nITEMS:",
+      itemsData
+    ); //
+  };
+
+  const moveItemDown = function (li) {
+    const i = itemsData.findIndex((item) => item.itemID === li.id);
+    const existingList = document.querySelector("#shoplist");
+
+    if (i !== itemsData.length - 1) {
+      const [movingDown, movingUp] = [itemsData[i], itemsData[i + 1]];
+      itemsData[i] = movingUp;
+      itemsData[i + 1] = movingDown;
+    }
+
+    existingList.innerHTML = "";
+    itemsData.forEach((item) => existingList.append(item.li));
+
+    console.log(
+      "ID of li to move down:",
+      li.id,
+      "\nIndex of li to move down:",
+      i,
+      "\nITEMS:",
+      itemsData
+    ); //
+  };
+
+  const removeItem = function (li) {
+    const i = itemsData.findIndex((item) => item.itemID === li.id);
+    const l = document.querySelector("#shoplist");
+
+    itemsData.splice(i, 1);
+    li.remove();
+    if (l.children.length === 0) {
+      l.remove();
+      itemIDGen = 0; // resets item ID generator
+      closeList();
+    }
+
+    console.log(
+      "ID of deleted li:",
+      li.id,
+      "\nIndex of deleted li:",
+      i,
+      "\nITEMS:",
+      itemsData,
+      "\nl.children.length:",
+      l.children.length
+    ); //
+  };
+
+  const toggleElementsVisibility = function (arr, onoff) {
+    switch (onoff) {
+      case 0:
+        arr.forEach((element) => element.classList.add("visibility-hidden"));
+        break;
+      case 1:
+        arr.forEach((element) => element.classList.remove("visibility-hidden"));
+    }
+  };
+
+  ///////////////////////////////////////////////
+
+  const p = productField.value;
+
+  if (p === "") {
+    alertMsg.innerHTML =
+      "<p>Por favor, insira seu produto no espaço apropriado acima.</p><br>";
+  } else {
+    if (alertMsg.innerHTML) alertMsg.innerHTML = "";
+    if (listContainer.classList.contains("hidden"))
+      listContainer.classList.remove("hidden");
+
+    if (itemsData.some((item) => item.itemName === p) && !allowDuplicate) {
+      openModal();
+    } else {
+      const listFormat = btnUl.classList.contains("format-btn-selected")
+        ? "ul"
+        : "ol";
+
+      // create elements
+      const newList = document.createElement(listFormat);
+      const existingList = document.querySelector("#shoplist");
+      const li = document.createElement("li");
+      const btnMoveItemUp = document.createElement("button");
+      const btnMoveItemDown = document.createElement("button");
+      const btnRemoveItem = document.createElement("button");
+      const liBtns = [btnMoveItemUp, btnMoveItemDown, btnRemoveItem];
+
+      const amount = Number(amountField.value) <= 0 ? "1" : amountField.value;
+      const product = amount === "" || amount === "1" ? p : `${p} (x${amount})`;
+
+      li.id = "li-" + itemIDGen;
+
+      itemsData.push({
+        itemID: li.id,
+        itemName: p,
+        itemAmount: amount,
+        li: li
+      });
+
+      // button setup
+      btnMoveItemUp.innerHTML = "&#8679";
+      btnMoveItemUp.title = "Mover item para cima";
+      btnMoveItemUp.classList.add("li-btn", "move-item", "visibility-hidden");
+
+      btnMoveItemDown.innerHTML = "&#8681";
+      btnMoveItemDown.title = "Mover item para baixo";
+      btnMoveItemDown.classList.add("li-btn", "move-item", "visibility-hidden");
+
+      btnRemoveItem.innerHTML = "&times;";
+      btnRemoveItem.title = "Remover item";
+      btnRemoveItem.classList.add("li-btn", "remove-item", "visibility-hidden");
+
+      // create new list/append new item
+      li.append(product, ...liBtns);
+      if (!existingList) {
+        newList.id = "shoplist";
+        newList.append(li);
+        listContainer.append(newList);
+
+        console.log(`existingList: ${existingList}
+        existingList_: ${document.querySelector("#shoplist")}`); //
+      } else existingList.append(li);
+
+      // toggle li button visibility on hover
+      li.addEventListener("mouseover", () => {
+        toggleElementsVisibility(liBtns, 1);
+      });
+
+      li.addEventListener("mouseleave", () => {
+        toggleElementsVisibility(liBtns, 0);
+      });
+
+      // set up li button functionality
+      btnMoveItemDown.addEventListener("click", () => moveItemDown(li));
+      btnMoveItemUp.addEventListener("click", () => moveItemUp(li));
+      btnRemoveItem.addEventListener("click", () => removeItem(li));
+
+      itemIDGen++; // increase id generator
+
+      // show info
+      console.log(
+        `=== INFO ===
+    
+      allowDuplicate: ${allowDuplicate}
+      existingList: ${existingList}
+      ITEMS:`,
+        itemsData
+      );
+    }
+  }
+};
+
+const showInfo = function () {
+  console.log("ITEMS:", itemsData);
+};
+
+/////////////////////////////////////////////////
+// EVENT LISTENERS
+/////////////////////////////////////////////////
 
 btnAddItem.addEventListener("click", addItem);
 
 btnClearList.addEventListener("click", () => {
   if (listContainer.children.length === 0) {
     alertMsg.innerHTML = "<p>Não há itens para remover!</p><br>";
-  } else {
-    listContainer.innerHTML = "";
-    items = [];
-    closeList();
-    showInfo(); //
-  }
+  } else openModal(modalMsgClearList);
 });
 
 btnUl.addEventListener("click", () => {
-  const existingList = document.querySelector("#shoplist");
-
-  /*   console.log("tag name: ", existingList.tagName); */
-  if (existingList !== null && existingList.tagName.toLowerCase() === "ol") {
-    const ul = document.createElement("ul"); // CONVERTER P FUNÇÃO
-    ul.id = existingList.id;
-    ul.innerHTML = existingList.innerHTML;
-    existingList.parentElement.replaceChildren(ul);
-  }
+  reformatList("ul");
   if (!btnUl.classList.contains("format-btn-selected")) {
     btnUl.classList.add("format-btn-selected");
     btnOl.classList.remove("format-btn-selected");
   }
+  showInfo(); //
 });
 
 btnOl.addEventListener("click", () => {
-  const existingList = document.querySelector("#shoplist");
-
-  if (existingList !== null && existingList.tagName.toLowerCase() === "ul") {
-    const ol = document.createElement("ol"); // CONVERTER P FUNÇÃO
-    ol.id = existingList.id;
-    ol.innerHTML = existingList.innerHTML;
-    existingList.parentElement.replaceChildren(ol);
-  }
+  reformatList("ol");
   if (!btnOl.classList.contains("format-btn-selected")) {
     btnOl.classList.add("format-btn-selected");
     btnUl.classList.remove("format-btn-selected");
   }
+  showInfo(); //
 });
 
 btnYes.addEventListener("click", () => {
   closeModal();
-  allowDuplicate = true;
-  addItem();
-  allowDuplicate = false;
+  if (modal.querySelector("p").innerText === modalMsgClearList) {
+    // clear list
+    listContainer.innerHTML = "";
+    itemsData = [];
+    itemIDGen = 0; // resets the id generator
+    closeList();
+    showInfo(); //
+  } else {
+    // insert duplicate item
+    allowDuplicate = true;
+    addItem();
+    allowDuplicate = false;
+  }
 });
 
 btnNo.addEventListener("click", closeModal);
-
-function showInfo() {
-  console.log(
-    `=== INFO ===
-
-  ITEMS:`,
-    items
-  );
-}
-
-/*
-BUGS/ERRORS: // these seem to be side effects of transcribing event-dependent content (such as the li buttons) via the innerHTML property
-- li buttons not showing up when list is set to a different format // maybe have a function to generate li buttons?
-- li button shenanigans when moving items up/down (buttons showing up permanently on translocated items, not showing up on those that were shifted from their init position)
-
-TODO:
-- implement li buttons with the format switching feature
-
-CONSIDER:
-- function to generate li buttons
-*/
